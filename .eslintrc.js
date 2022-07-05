01072309500ff3389d738a53015a9b4a66eee519
mate444 <mateoeo23@gmail.com>
2022-07-04 23:42:53 -0300

    Added database connection

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    'security'
   ],
   root: true,
   env: {
@@ -21,5 +21,15 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    semi: [2, "always"],
+    camelcase: "off",
+    quotes: "off",
+    'no-unused-vars': "warn",
+    'max-len': ["error", { "code": 120,
+        ignoreComments: true}],
+    'no-console': "warn",
+    'quote-props': ["off", { "unnecesary": false}],
+    'no-tabs': ["off"],
+    'no-mixed-spaces-and-tabs': ["off"]
   },
 };
