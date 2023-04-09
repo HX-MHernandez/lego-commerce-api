d4508067d3093ca3455bed3a1b910604753fea3b
mate444 <mateoeo23@gmail.com>
2023-04-09 14:22:12 +0200

    Fixed bug on timestamp

---
+++ src/product/entities/Product.entity.ts
@@ -0,0 +1,77 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToMany,
+  JoinTable,
+  OneToMany,
+  ManyToOne
+} from 'typeorm';
+import { Cart } from '../../models/Cart.entity';
+import { Category } from '../../models/Category.entity';
+import { Order } from '../../models/Order.entity';
+import { Review } from '../../models/Review.entity';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+    productId: string;
+
+  @Column({ type: 'varchar', length: '30' })
+    name: string;
+
+  @Column('decimal')
+    price: number;
+
+  @Column({ type: 'int' })
+    pieces: number;
+
+  @Column('uuid')
+    categoryId: string;
+
+  @Column('float')
+    weight: number;
+
+  @Column('text')
+    image: string;
+
+  @Column('int')
+    stock: number;
+
+  @Column('float', { nullable: true })
+    discount: number;
+
+  @Column('int', { nullable: true })
+    priceDiscount: number;
+
+  @Column('timestamp', { default: () => 'NOW()' })
+    creation_date: string;
+
+  @Column('int')
+    numInstructions: number;
+
+  @Column('int')
+    numMinifigures: number;
+
+  @Column('float')
+    rating: number;
+
+  @Column({ type: 'varchar', length: '20' })
+    size: string;
+
+  @ManyToMany(() => Category, (category) => category.categoryId, {
+    onDelete: 'RESTRICT'
+  })
+
+  @JoinTable()
+    categories: Category[];
+
+  @OneToMany(() => Review, (review) => review.product)
+    reviews: Review[];
+
+  @OneToMany(() => Order, (order) => order.product)
+    orders: Order[];
+
+  @ManyToOne(() => Cart, (cart) => cart.products)
+    cart: Cart;
+}
